02289eb35164206ffea68a62a1faa9e364f5ce9a
wesmelo <wsfmelo@gmail.com>
2018-05-25 19:18:18 -0300

    ajustado pontos conforme aula de revisao

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -54,30 +54,32 @@
   */
 
   function moreThan (age){
-    if(person.age > age){
-      return true;
-    }
-    return false;
-  }
+  //  if(person.age > age){
+  //    return true;
+  //  }
+  //  return false;
+  //}
+  return person.age > age;
+}
 
-  console.log( 'The person has more than 25 years old?', moreThan(idade) );
+  console.log( 'The person has more than 25 years old?', moreThan(25) );
 
   /*
   Faça um loop de 0 a 20, que adicione cada número como um item de um
   array chamado `numbers`. Se o contador for maior que 10, saia do loop.
   Mostre no console os números no array.
   */
+  console.log( 'De 0 a 10:', numbers );
 
   var numbers = [];
-
-  for (var i = 0; i <= 20; i++) {
-    if(numbers.length > 10){
+  for ( var i = 0; i <= 20; i++ ) {
+    if( i > 10 ){
       break;
     }
     numbers.push(i);
   };
+  console.log( numbers );
 
-  console.log( 'De 0 a 10:', numbers );
 
   /*
   Faça outro loop de 0 a 20, que adicione a um array chamado `numbers` (já
@@ -85,7 +87,9 @@
   esses. Se o número for ímpar, pular para o próximo número.
   Mostrar no console os números do array.
   */
-  var numbers = [];
+  console.log( 'Pares de 0 a 20:' );
+
+  numbers = [];
 
   for (var i = 0; i <= 20; i++) {
     if (i % 2 !== 0) {
@@ -94,5 +98,5 @@
     numbers.push(i);
   };
 
-  console.log( 'Pares de 0 a 20:', numbers );
+  console.log( numbers );
 })();
